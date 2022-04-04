5f1916b44e1502740ddf4a10c6f60f56cadc013f
slycreator <chinonso1amaechi@gmial.com>
2022-04-04 03:36:05 +0100

    added map

---
+++ screens/MeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const MeScreen = () => {
+    return (
+        <View>            <Text>Hello Me</Text>
+        </View>
+    )
+}
+
+export default MeScreen
---
+++ screens/SearchHospitalScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import Input from '../components/Input'
+import tw from 'twrnc'
+import { MaterialIcons } from '@expo/vector-icons';
+import { StackActions, useNavigation } from '@react-navigation/core'
+import Map from '../components/Map'
+const SearchHospitalScreen = () => {
+
+    const { dispatch } = useNavigation();
+    const onTap = () => {
+        dispatch(StackActions.push('Map'))
+    }
+    return (
+        <View style={tw`flex-1 bg-white px-6 py-4 justify-between`}>
+            <Input title="What is your address" />
+
+            <TouchableOpacity onPress={onTap}>
+                <View style={tw`  items-center `}>
+                    <MaterialIcons name="add-location-alt" size={60} color="#08857C" />
+                    <Text style={tw`text-base font-bold p-2`}>Use G.P.S</Text>
+                    <Text style={tw`text-sm text-gray-500`}>Tap to use current location</Text>
+                </View>
+            </TouchableOpacity>
+
+            <View style={tw``}>
+                <Text style={tw`text-base pb-2 font-bold text-gray-700`}>Hospital Near You</Text>
+                <View style={tw`h-70 rounded-lg`}>
+                    <Map />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default SearchHospitalScreen
